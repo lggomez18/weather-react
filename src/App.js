e8cb0d6a9c9d06e37289cc8aea12bd297a7dd602
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-07-15 22:46:04 -0600

    Testing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Weather App</h1>
+        <h1>Weather App Test</h1>
         <Weather city="Paris"/>
       </header>
     </div>
